8de9b3b5e936211a7a6aebf2a6997d331623561a
dantal2000 <dantal2000@yandex.ru>
2016-10-31 12:55:16 +0300

    Version 5.1 is there
    Created panel of TO_DO
    Added static strings (not all; and just ru)
    Some bugs are fixed

---
+++ src/GUI001_f/Data_Strings.java
@@ -0,0 +1,10 @@
+package GUI001_f;
+
+/**
+ * Created by Даниил on 31.10.2016.
+ */
+public class Data_Strings {
+    final public static String a = "Получить куки";
+    final public static String b = "Открыть";
+    final public static String c = "Получить заголовки ответа сервера";
+}
